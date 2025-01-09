8135fc278a5f1cc46e0bc6caa6bd9cc4744d344b
Dhinesh <dhinesh4460@gmail.com>
2025-01-09 19:02:27 +0530

    text change

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -24,7 +24,7 @@ const Signup = () => {
     email: Yup.string().email("Invalid email address").required("Email is required"),
     password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
     repassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], "Passwords must match")
+      .oneOf([Yup.ref('password'), null], "Password must match")
       .required("Confirm password is required"),
   });
 
